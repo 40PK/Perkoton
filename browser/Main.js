3c697aeb56070ab96a32b4de215dc7823d042370
Perkovec <perkovec24@gmail.com>
2016-04-15 18:06:34 +0300

    Remove unused packages, double quotes, scss support

--- browser/Main.js
+++ browser/Main.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React from "react";
 
 class Main extends React.Component {
   render() {
--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,13 @@
 var gulp = require( "gulp" );
 var run = require( "gulp-run" );
 var clean = require( "gulp-clean" );
-var sourcemaps = require( "gulp-sourcemaps" );
 var browserify = require( "browserify" );
 var babelify = require('babelify');
 var source = require( "vinyl-source-stream" );
+var sass = require('gulp-sass');
+var cleanCSS = require('gulp-clean-css');
+var concatCss = require('gulp-concat-css');
+var prefixer = require('gulp-autoprefixer');
 
 gulp.task( "clean", function (){
 
@@ -13,28 +16,39 @@ gulp.task( "clean", function (){
 
 });
 
-gulp.task( 'browserify', [ "clean" ], function (){
+gulp.task( "browserify", [ "clean" ], function (){
 
-	var bundler = browserify( './browser/app.js' )
+	var bundler = browserify( "./browser/app.js" )
 					.transform( babelify, { presets: [ "es2015", "react" ] } );
 
 	return bundler.bundle()
-		.pipe( source( 'app.js' ) )
-		.pipe( gulp.dest( './package' ) );
+		.pipe( source( "app.js" ) )
+		.pipe( gulp.dest( "./package" ) );
 
 } );
 
 
 gulp.task( "build:html", [ "browserify" ], function (){
 
-	return gulp.src( [ 'browser/*.html', 'browser/**/*.html' ], { base: "." } )
+	return gulp.src( [ "browser/*.html", "browser/**/*.html" ], { base: "." } )
 			   .pipe( gulp.dest( "package" ) );
 
 } );
 
-gulp.task( "build", [ "build:html" ], function (){
+gulp.task( "build:styles", function () {
 
-	return gulp.src( [ 'app/**/*', 'package.json' ], { base: "." } )
+    gulp.src( "browser/styles/*" ) 
+        .pipe( sass() )
+        .pipe( prefixer() ) 
+        .pipe( concatCss( "main.css" ) )
+        .pipe( cleanCSS() )
+        .pipe( gulp.dest( "package/browser/" ) );
+
+} );
+
+gulp.task( "build", [ "build:html", "build:styles" ], function (){
+
+	return gulp.src( [ "app/**/*", "package.json" ], { base: "." } )
 			   .pipe( gulp.dest( "package" ) );
 
 } );
