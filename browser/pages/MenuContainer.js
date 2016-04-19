b48fbab6a023b89dc16e8125fa755bdd39a6dd50
Perkovec <perkovec24@gmail.com>
2016-04-19 22:53:20 +0300

    Left menu container

---
+++ browser/pages/MenuContainer.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+class MenuContainer extends React.Component {
+
+  	render (){
+    	return (
+  			<div className="menu-container">
+  				
+  			</div>
+  		);
+  	}
+
+}
+
+export default MenuContainer;
--- browser/pages/Player.js
+++ browser/pages/Player.js
@@ -2,13 +2,20 @@ import React from "react";
 
 import TitleBarButtons from "./components/TitleBarButtons";
 
+import MenuContainer from "./MenuContainer";
+
 class Player extends React.Component {
 
   render() {
-    return <TitleBarButtons/>;
+    return ( 
+    	<div>
+    		<MenuContainer/>
+    		<TitleBarButtons/>
+    	</div>
+    );
 
   }
-  
+
 }
  
 export default Player;
